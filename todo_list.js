2624ed96a1bff4d7bdc297b906d4ec8de2992160
eshuster <eshuster3@gmail.com>
2015-04-13 14:28:49 -0700

    SS4

--- todo_list.js
+++ todo_list.js
@@ -1,10 +1,40 @@
-var newTodoList = function() {
-	// ???
+
+var TodoList = function() {
+  this.tasks = []
 };
 
+// var todoList = new TodoList();
 
+var Task = function(id, description){
+  this.id = id
+  this.description = description;
+  this.completed = false;
+};
 
-// Driver code
+TodoList.prototype.add = function(item) {
+  id = this.tasks.length + 1
+  var newTask = new Task(id, item);
+  this.tasks.push(newTask);
+};
 
+TodoList.prototype.list = function(){
+  for (i=0; i<this.tasks.length; i++){
+    console.log("id: " + i + " ");
+    console.log(this.tasks[i]);
+  };
+};
 
-var todoList = newTodoList();
+Task.prototype.complete = function() {
+  this.completed = true;
+};
+
+Task.prototype.remove = function(){
+  for (var i = 0; i < this.tasks.length; i++){
+    if (this.tasks[i] === this){
+      delete this.tasks[i];
+    };
+  };
+};
+
+var todoList = new TodoList();
+
