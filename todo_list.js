9cd6497e46f63b149502a51bb22d92c55a5dab93
Hugh Sutton-Gee & Katrina Eaton <pair+hsuttong.k-eaton@devbootcamp.com>
2015-04-13 15:12:47 -0700

    initial commit

--- todo_list.js
+++ todo_list.js
@@ -1,10 +1,60 @@
-var newTodoList = function() {
-	// ???
+"use strict";
+
+var TodoList = function () {
+  this.tasks = [];
+  this.add = function (task) {
+    this.tasks.push({description: task, completed: false})
+  };
+
+  this.list = function (){
+    for(var i=0; i<this.tasks.length; i++) {
+      console.log(this.tasks[i]);
+    }
+  };
 };
 
 
 
+
 // Driver code
+//var todoList = newTodoList();
+
+// Build this API:
+// Note we are using a JavaScript constructor now.
+var groceryList = new TodoList();
+groceryList.add('bread');
+groceryList.add('cheese');
+groceryList.add('milk');
+
+// tasks is now an array of Task objects
+groceryList.tasks //-> [Task, Task, Task]
+
+groceryList.list();
+//> Task {id: 1, description: 'bread', completed: false}
+//> Task {id: 2, description: 'cheese', completed: false}
+//> Task {id: 3, description: 'milk', completed: false}
 
 
-var todoList = newTodoList();
+// getting a task object
+var breadTask = groceryList.tasks[0];
+
+breadTask.id //-> 1 (some unique numerical ID)
+breadTask.description //-> 'bread'
+breadTask.completed //-> false
+
+
+// This should complete the task
+breadTask.complete();
+
+groceryList.list();
+//> Task {id: 1, description: 'bread', completed: true}
+//> Task {id: 2, description: 'cheese', completed: false}
+//> Task {id: 3, description: 'milk', completed: false}
+
+
+// This should remove the task from the todo list
+breadTask.remove();
+
+groceryList.list();
+//> Task {id: 2, description: 'cheese', completed: false}
+//> Task {id: 3, description: 'milk', completed: false}
