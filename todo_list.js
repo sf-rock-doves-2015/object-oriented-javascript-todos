05f367ae45dab9d493e338ed7ddec1a89f976968
gwmartin <ginny.w.martin@gmail.com>
2015-04-13 17:11:47 -0700

    completed

--- todo_list.js
+++ todo_list.js
@@ -1,10 +1,83 @@
-var newTodoList = function() {
-	// ???
+var TodoList = function() {
+  this.tasks = [];
+
 };
 
+var Task = function(item, list){
+  this.description = item;
+  this.completed = false;
+  this.id = Task.lastID++;
+  this.list = list;
+
+};
+
+Task.lastID = 1;
+
+TodoList.prototype.add = function(item){
+  this.tasks.push(new Task(item, this));
+};
+
+TodoList.prototype.list = function(){
+  for(var i=0; i < this.tasks.length; i++) {
+    console.log(this.tasks[i]);
+  };
+};
+
+Task.prototype.complete = function(){
+  this.completed = true;
+};
+
+Task.prototype.remove=function(){
+  var index = this.list.tasks.indexOf(this)
+  this.list.tasks.splice(index,1);
+};
+
+// TodoList.prototype.list = function(){
+//   for(var i=0; i < this.tasks.length; i++){
+//     for (var task in this.tasks){
+//       task.id = i;
+//   }
+// } Don't do this, assign a task id each time create a new task instance
 
 
-// Driver code
+
+var groceryList = new TodoList();
+groceryList.add('bread');
+groceryList.add('cheese');
+groceryList.add('milk');
+
+// tasks is now an array of Task objects
+groceryList.tasks //-> [Task, Task, Task]
+
+groceryList.list();
+//> Task {id: 1, description: 'bread', completed: false}
+//> Task {id: 2, description: 'cheese', completed: false}
+//> Task {id: 3, description: 'milk', completed: false}
 
 
-var todoList = newTodoList();
+// getting a task object
+var breadTask = groceryList.tasks[0];
+
+breadTask.id //-> 1 (some unique numerical ID)
+breadTask.description //-> 'bread'
+breadTask.completed //-> false
+
+
+// This should complete the task
+breadTask.complete();
+
+groceryList.list();
+//> Task {id: 1, description: 'bread', completed: true}
+//> Task {id: 2, description: 'cheese', completed: false}
+//> Task {id: 3, description: 'milk', completed: false}
+
+
+// This should remove the task from the todo list
+breadTask.remove();
+
+groceryList.list();
+//> Task {id: 2, description: 'cheese', completed: false}
+//> Task {id: 3, description: 'milk', completed: false}
+
+
+
